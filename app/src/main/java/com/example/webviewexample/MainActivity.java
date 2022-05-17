5201c4aa3cd82dd8ab075d06a581a469d531f758
Kirill <cyrillepishin@gmail.com>
2022-05-17 18:07:57 +0300

    Initial commit

---
+++ app/src/main/java/com/example/webviewexample/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.webviewexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        start();
+    }
+
+    private void start() {
+        webView = findViewById(R.id.webView);
+        webView.setWebViewClient(new MyWebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl(getString(R.string.URL));
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
